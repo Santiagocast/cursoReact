f917113ae7830f163014bdd19df74630f73b1bcd
Santiagocast <santiagojcastelli@gmail.com>
2022-06-07 03:29:01 -0300

    Arreglo loadings en itemdetails

--- src/components/itemDetailContainer/itemDetailContainer.js
+++ src/components/itemDetailContainer/itemDetailContainer.js
@@ -2,18 +2,29 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProduct } from "../asyncmock";
 import ItemDetail from "../itemDetail/itemDetail";
+import Spinner from "../Spinner/spinner";
 
 const ItemDetailContainer = () =>{
     const [product, setProduct] = useState([])
     const [ pictures, setPictures] = useState([])
     const {productId} = useParams()
+    const [loading, setLoading] = useState()
+
+
     useEffect( ()=>{
+        setLoading(true)
         getProduct(productId).then( (res) => {
             setProduct(res)
             setPictures(res.pictureUrl)
             console.log(res);
-        })
+        }).finally(()=> setLoading(false))
     },[])
+
+    if(loading){
+        return( <Spinner></Spinner>)
+    }
+
+
     return(
         <div>
             <ItemDetail pictureUrl= {pictures} {...product}></ItemDetail>
